88a2320dab58f6a41c9c44a07c42b1175ec62efc
shuichis929 <shuichis929@gmail.com>
2022-10-17 19:55:53 +0900

    showアクションの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,7 @@
 class UsersController < ApplicationController
-  def update
-    if @user.update(user_params)
-      redirect_to root_path
-    else
-      render :edit
-    end
+
+  def show
+    @user = current_user
   end
 
-  private
-
-  def user_params
-    params.require(:user).permit(:nickname, :name, :avatar, :introduction, :email, :password)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
 
 end
