0e5e304bd57f153d667041545c9a9864b0553dc2
shuichis929 <shuichis929@gmail.com>
2022-10-19 11:22:09 +0900

    usersコントローラーのアクションの定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:edit, :update]
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(current_user)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :name, :avatar, :introduction)
+  end
 end
