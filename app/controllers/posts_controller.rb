37325c55e4127ca1298e2bf77234675dafade619
shuichis929 <shuichis929@gmail.com>
2022-10-19 23:14:23 +0900

    postsコントローラーのcreateアクションの定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,18 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.create(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render new_post_path(@item)
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:image, :text).merge(user_id: current_user.id)
+  end
 end
