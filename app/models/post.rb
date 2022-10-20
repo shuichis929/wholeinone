c6c70ccb9b277364cf27dec49f727c7648108501
shuichis929 <shuichis929@gmail.com>
2022-10-20 19:09:07 +0900

    postモデルのバリデーションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user
   has_one_attached :post_image
 
-  validates :text, presence: true
+  validates :text, presence: true, length: { maximum: 200 }
   validates :post_image, presence: true
 end
