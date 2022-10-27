21d974a4965396bd94538f770f2d2e912d75b548
shuichis929 <shuichis929@gmail.com>
2022-10-27 15:47:40 +0900

    ActiveHashのconditionモデル内の記述修正

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -1,4 +1,4 @@
-class condition < ActiveHash::Base
+class Condition < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
     { id: 2, name: '新品・未使用' },
@@ -11,5 +11,5 @@ class condition < ActiveHash::Base
 
   include ActiveHash::Associations
   has_many :items
-  
+
   end
