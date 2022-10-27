968fced96f645fb216224d10bb97d0b95fcdc17e
shuichis929 <shuichis929@gmail.com>
2022-10-27 15:19:01 +0900

    ActiveHashのモデル作成

---
+++ app/models/category.rb
@@ -0,0 +1,14 @@
+class category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: 'ドライバー' },
+    { id: 3, name: 'フェアウェイウッド' },
+    { id: 4, name: 'ユーティリティー' },
+    { id: 5, name: 'アイアン' },
+    { id: 6, name: 'ウェッジ' },
+    { id: 7, name: 'パター' },
+    { id: 8, name: 'アパレル' },
+    { id: 9, name: 'アクセサリー' },
+    { id: 10, name: 'その他' }
+  ]
+  end
---
+++ app/models/days_to_ship.rb
@@ -0,0 +1,8 @@
+class DaysToShip < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '１〜２日で発送' },
+    { id: 3, name: '２〜３日で発送' },
+    { id: 4, name: '４〜７日で発送' }
+  ]
+end
