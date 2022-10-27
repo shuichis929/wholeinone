a8718152237d9d15252632e041ec828683e0b3fc
shuichis929 <shuichis929@gmail.com>
2022-10-27 15:28:13 +0900

    itemモデルのバリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,16 @@ class Item < ApplicationRecord
   belongs_to :delivery_charge
   belongs_to :prefecture
   belongs_to :days_to_ship
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :delivery_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank" }
 end
