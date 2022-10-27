51f1919cd0cba2de936179bcd35716b79aa74f60
shuichis929 <shuichis929@gmail.com>
2022-10-27 15:26:46 +0900

    itemモデルのバリデーションとアソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ApplicationRecord
-  
+  belongs_to :user
+  has_one_attached :item_image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_charge
+  belongs_to :prefecture
+  belongs_to :days_to_ship
 end
