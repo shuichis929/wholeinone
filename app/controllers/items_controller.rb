8237beeae290287bb2b3ac24b5195176d30134e4
shuichis929 <shuichis929@gmail.com>
2022-10-31 15:18:01 +0900

    indexアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
 
+  def index
+    @item = Item.order('created_at DESC')
+  end
+
   def new
     @item = Item.new
   end
