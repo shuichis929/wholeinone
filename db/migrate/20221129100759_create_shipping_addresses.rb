20b565d3d5696734ab3d625d208f659f6630d1b7
shuichis929 <shuichis929@gmail.com>
2022-11-29 23:09:13 +1300

    shipping_addressモデルのマイグレーションファイルの定義

--- db/migrate/20221129100759_create_shipping_addresses.rb
+++ db/migrate/20221129100759_create_shipping_addresses.rb
@@ -1,7 +1,13 @@
 class CreateShippingAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_addresses do |t|
-
+      t.string     :post_code,     null: false
+      t.integer    :prefecture_id, null: false
+      t.string     :city,          null: false
+      t.string     :block,         null: false
+      t.string     :building
+      t.string     :phone_number,  null: false
+      t.references :purchase,      null: false, foreign_key: true
       t.timestamps
     end
   end
