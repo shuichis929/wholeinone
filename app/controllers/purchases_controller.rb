0446241ecf2bb1e962e8f2e8d96a52ff4d578459
shuichis929 <shuichis929@gmail.com>
2022-11-29 23:24:17 +1300

    purchaseコントローラーの定義

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,41 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!, only: :index
+  before_action :set_item, only: [:index, :create]
+
+  def index
+    @purchase_shipping_address = PurchaseShippingAddress.new
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase.present?
+  end
+
+  def create
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
+    if @purchase_shipping_address.valid?
+      pay_item
+      @purchase_shipping_address.save
+      redirect_to root_path
+    else
+      render 'purchases/index'
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping_address).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
