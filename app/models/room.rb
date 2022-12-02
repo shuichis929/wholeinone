51ff66ed5d36fb1ff471ba17e092a67179516752
shuichis929 <shuichis929@gmail.com>
2022-12-02 17:43:55 +1300

    roomモデルのバリデーションの修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,5 +3,5 @@ class Room < ApplicationRecord
   has_many :users, through: :room_users
   has_many :messages, dependent: :destroy
 
-  validates :name, presence: true
+  validates :room_name, presence: true
 end
