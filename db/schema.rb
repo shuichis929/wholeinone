76addaec6f0013cd4e0c85411395c176c81d4bc0
shuichis929 <shuichis929@gmail.com>
2022-12-02 17:17:07 +1300

    room_userモデルのマイグレーションファイルの定義

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_02_041237) do
+ActiveRecord::Schema.define(version: 2022_12_02_041516) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,15 @@ ActiveRecord::Schema.define(version: 2022_12_02_041237) do
     t.index ["user_id"], name: "index_purchases_on_user_id"
   end
 
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
+  end
+
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "room_name", null: false
     t.datetime "created_at", precision: 6, null: false
@@ -116,5 +125,7 @@ ActiveRecord::Schema.define(version: 2022_12_02_041237) do
   add_foreign_key "messages", "users"
   add_foreign_key "purchases", "items"
   add_foreign_key "purchases", "users"
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
   add_foreign_key "shipping_addresses", "purchases"
 end
