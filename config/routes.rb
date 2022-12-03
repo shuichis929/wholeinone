3b4dfab1f9f71a84acf4e6cf6b37e95db19c94c9
shuichis929 <shuichis929@gmail.com>
2022-12-03 23:21:41 +1300

    ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,6 @@ Rails.application.routes.draw do
   resources :items do
     resources :purchases, only: [:index, :create]
   end
-  resources :rooms, only: [:new, :create, :destroy] do
-    resources :messages, only: [:index, :create]
-  end
+  resources :rooms, only: [:new, :create, :destroy]
 
 end
