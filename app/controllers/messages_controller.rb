ec14307f4c69bbfcd0494363e4762afbb12944af
shuichis929 <shuichis929@gmail.com>
2022-12-05 18:29:58 +1300

    messageコントローラーの編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,18 @@
 class MessagesController < ApplicationController
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    @message.save
+    if @message.save
+      redirect_to room_path(@room.id)
+    else
+      render template: "rooms/show"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  end
 end
