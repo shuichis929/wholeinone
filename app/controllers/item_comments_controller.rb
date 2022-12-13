8b02118dbab9167f9cfab0ca87ae75326257bae5
shuichis929 <shuichis929@gmail.com>
2022-12-13 19:56:36 +1300

    item_commentsコントローラーの編集

--- app/controllers/item_comments_controller.rb
+++ app/controllers/item_comments_controller.rb
@@ -1,2 +1,15 @@
 class ItemCommentsController < ApplicationController
+  before_action :authenticate_user!, only: :create
+
+  def create
+    @item_comment = ItemComment.new(item_comment_params)
+    if @item_comment.save
+      redirect_to item_path(params[:item_id])
+    end
+  end
+
+  private
+  def item_comment_params
+    params.require(:item_comment).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
