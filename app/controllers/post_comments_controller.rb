dfe7b434a065df2129ba6e0e5ada75052e60cec4
shuichis929 <shuichis929@gmail.com>
2022-12-13 14:53:11 +1300

    post_commentsコントローラーの編集

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,2 +1,13 @@
 class PostCommentsController < ApplicationController
+  def create
+    @post_comment = PostComment.new(post_comment_params)
+    if @post_comment.save
+      redirect_to post_path(params[:post_id])
+    end
+  end
+
+  private
+  def post_comment_params
+    params.require(:post_comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
